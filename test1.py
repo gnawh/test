dd577736f881fc98d7e2112a07c971edb1d57c28
Hoyong Hwang <hwanghy@gmail.com>
2014-08-22 00:03:22 -0500

    Initial project

---
+++ test1.py
@@ -0,0 +1,15 @@
+__author__ = 'hoyong'
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def main ():
+    print "Hello PyCharm"
+    a = 1
+    b = [1,2,3]
+
+    plt.plot(b)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
